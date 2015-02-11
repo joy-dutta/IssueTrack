f85173fab49888c85422fc1f93e1e0ee54b5d01f
pramita <pramita@pramita>
2015-02-11 16:14:33 +0530

    sys out inserted pramita

--- ITS/app/controllers/Application.java
+++ ITS/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-	  System.out.println("Hiiii");
+	  System.out.println("Hiiii Pramita");
 	  
     return ok(index.render("Pramita's new application is not ready."));
   }
