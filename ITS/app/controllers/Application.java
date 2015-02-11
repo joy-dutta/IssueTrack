168ae4d6ede14bd9c7021c3701024f127fb14d77
pramita <pramita@pramita>
2015-02-11 13:44:55 +0530

    Print message

--- ITS/app/controllers/Application.java
+++ ITS/app/controllers/Application.java
@@ -8,6 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
+	  System.out.println("Hello World");
     return ok(index.render("Pramita's new application is not ready."));
   }
   
