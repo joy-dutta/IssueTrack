e8a1f0b879b9c4db71f0daa7b099dc5315fc1d1c
Joy Dutta <joyd@egnaroinc.com>
2015-02-18 11:50:53 +0530

    Extra class deleted

--- ITS/app/controllers/Application.java
+++ ITS/app/controllers/Application.java
@@ -8,8 +8,8 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-	  System.out.println("Hello Papia");
-    return ok(index.render("Papia new play application is ready."));
+	  System.out.println("Hello Joy");
+    return ok(index.render("Joy's new play application is ready."));
 
   }
   
